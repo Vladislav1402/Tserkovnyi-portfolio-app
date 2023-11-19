f7a7343602782a525a01d62854805eb9dde02148
Vladislav <alibaba14.ua@gmail.com>
2023-11-19 16:03:44 +0100

    feat(All): Created header component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
+import { RandomColors } from './components/portfolio/random-colors/random-colors-overview/random-colors-overview.component';
 import { Routes } from '@angular/router';
+import { InnerWrapper } from './components/inner-wrapper/inner-wrapper.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: InnerWrapper,
+        children: [
+            {
+                path: 'portfolio',
+                component: RandomColors
+            }
+        ]
+    }
+];
